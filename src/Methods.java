1bd22698ac5599822edc5eef06b0ee6d629607fb
IVAN <luka.news.2001@mail.ru>
2023-03-06 15:16:20 +0300

    Домашнее задание: Методы - Исправлено!

--- src/Methods.java
+++ src/Methods.java
@@ -1,45 +1,22 @@
 import java.time.LocalDate;
 public class Methods {
     public static void main(String[] args) {
+
+        var result = returnTheTotalNumberOfDeliveryDays(50);
         System.out.println("Задача 1");
-        var result = howMachDeliveryDaysNeed(98);
         if (result == -1) {
             System.out.println("Доставки нет");
         } else {
             System.out.println("Доставка займёт " + result + " дня");
         }
 
-        int a = 2020;
-        printLeapYear(2000);
+        int year = 2021;
+        printLeapYear(year);
 
         printVersion(1, 2022);
     }
 
-    private static void printLeapYear(int year) {
-        System.out.println("Задача 2");
-        year = 2021;
-        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-            System.out.println(year + " год является високосным");
-        } else {
-            System.out.println(year + " год не является високосным");
-        }
-    }
-
-    private static void printVersion(int os, int year) {
-        System.out.println("Задача 3");
-        int currentYear = LocalDate.now().getYear();
-        if (currentYear == year && os == 1) {
-            System.out.println("Установите обычную версию для Android");
-        } else if (currentYear > year && os == 1) {
-            System.out.println("Установите облегчённую версию для Android");
-        } else if (currentYear == year && os == 0) {
-            System.out.println("Установите обычную версию для iOC");
-        } else {
-            System.out.println("Установите облегчённую версию для iOC");
-        }
-    }
-
-    private static int howMachDeliveryDaysNeed(int deliveryDistance) {
+    public static int returnTheTotalNumberOfDeliveryDays(int deliveryDistance) {
         if (deliveryDistance > 100) {
             return - 1;
         }
@@ -51,5 +28,28 @@ public class Methods {
             deliveryDay++;
         }
         return deliveryDay;
+    }
+
+    public static void printLeapYear(int year) {
+        System.out.println("Задача 2");
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+    }
+
+    public static void printVersion(int os, int year) {
+        System.out.println("Задача 3");
+        int currentYear = LocalDate.now().getYear();
+        if (currentYear == year && os == 1) {
+            System.out.println("Установите обычную версию для Android");
+        } else if (currentYear > year && os == 1) {
+            System.out.println("Установите облегчённую версию для Android");
+        } else if (currentYear == year && os == 0) {
+            System.out.println("Установите обычную версию для iOC");
+        } else {
+            System.out.println("Установите облегчённую версию для iOC");
+        }
     }
 }
